420b0e44771d63a7a70c0c26bd48820eb4b7c76c
edoardope <pencoedoardo@gmail.com>
2023-05-08 02:11:18 +0200

    step 1 finito

---
+++ src/components/CardCont.vue
@@ -0,0 +1,36 @@
+<script>
+import SingleCard from './SingleCard.vue'
+import { store } from '../store';
+
+export default {
+  name: "CardCont",
+  components: {
+    SingleCard,
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <main>
+    <SingleCard v-for="(element, index) in store.yuGiOhCardsData" :key="index" :dettagliCarta="element" :cindex="index" />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  height: 100%;
+  width: 100%;
+  background-color: white;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: "SingleCard",
+    props: {
+        dettagliCarta: Object,
+        cindex: Number
+    },
+
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<template>
+    <div>
+        <img :src="dettagliCarta.card_images[0].image_url" alt="">
+        <br>
+        <span class="text-white h5">{{ dettagliCarta.name }}</span>
+        <br>
+        <span>{{ dettagliCarta.type }}</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+    text-align: center;
+    flex-basis: calc(90% / 3);
+    margin-bottom: 10px;
+    background-color: orange;
+
+    span {
+        display: inline-block;
+        margin: 10px;
+    }
+
+    img {
+        width: 100%;
+    }
+}
+</style>
